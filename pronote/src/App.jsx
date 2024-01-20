e96140a6e427141f69624a863fd5ecb661aab570
Meghana <meghanamanchal@gmail.com>
2024-01-20 17:30:26 +0530

    added Editor.jsx

---
+++ pronote/src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './App.css';
+import Editor from './Components/Editor';
+
+function App() {
+  return (
+    <div className="App">
+    <Editor />
+    </div>
+  );
+}
+
+export default App;
